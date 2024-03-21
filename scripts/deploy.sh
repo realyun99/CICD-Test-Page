32f55dbfe2e0d05ebd98fc6f9f39d52a120b01ee
realyun99 <realyun99@gmail.com>
2024-03-21 14:58:08 +0900

    modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/app/*-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build cicd-test: $JAR_NAME" >> /home/ec2-user/app/deploy.log
+BUILD_PATH=$(ls /home/ec2-user/build/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사" >> /home/ec2-user/app/deploy.log
+echo "> build 파일 복사"
 DEPLOY_PATH=/home/ec2-user/
-cp $BUILD_JAR $DEPLOY_PATH
+cp $BUILD_PATH $DEPLOY_PATH
 
 echo "> cicd-test.jar 교체"
 CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
@@ -14,17 +14,17 @@ APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
 
 ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/app/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/app/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $APPLICATION_JAR_NAME 배포"    >> /home/ec2-user/app/deploy.log
-nohup java -jar $APPLICATION_JAR_NAME >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+echo "> $APPLICATION_JAR 배포"
+nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
